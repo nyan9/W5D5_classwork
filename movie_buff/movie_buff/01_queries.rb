895c81e3d0f5926ce88c4963a7118f7eff810a0d
Hank <hankchen1997@yahoo.com>
2021-01-29 15:18:30 -0500

    Finished 01spec

--- movie_buff/movie_buff/01_queries.rb
+++ movie_buff/movie_buff/01_queries.rb
@@ -20,7 +20,11 @@ def harrison_ford
   #
   # Find the id and title of all movies in which Harrison Ford
   # appeared but not as a lead actor
-  Movie.select(:id, :title).joins(:actors).where.not("ord = 1").where(actors: {name: "Harrison Ford"})
+  Movie
+    .select(:id, :title)
+    .joins(:actors) 
+    .where.not("ord = 1")
+    .where(actors: {name: "Harrison Ford"})
 end
 
 def biggest_cast
@@ -37,7 +41,9 @@ def biggest_cast
   #
   # Find the id and title of the 3 movies with the
   # largest casts (i.e most actors)
+  
 
+    Movie.select(:id, :title).joins(:castings).group('movies.id').order("COUNT(*) DESC").limit(3)
 end
 
 def directed_by_one_of(them)
@@ -48,10 +54,11 @@ def directed_by_one_of(them)
   # We can use IN to test if an element is present in an array.
   #
   # ActiveRecord gives us an even better way to write this:
-  #
+  
   # Movie.where(yr: years)
   #
   # Find the id and title of all the movies directed by one of 'them'.
+    Movie.select(:id, :title).joins(:director).where("actors.name IN (?)", them)
 
 end
 
@@ -66,5 +73,5 @@ def movie_names_before_1940
   # improve performace for larger queries.
   #
   # Use pluck to find the title of all movies made before 1940.
-
+    Movie.where('yr < 1940').pluck(:title)
 end
